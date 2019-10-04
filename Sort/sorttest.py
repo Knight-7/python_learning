d80461ab94b90dbfcae2d3a61a756fbb6e6ab93e
knight <1252809618@qq.com>
2019-10-04 13:59:47 +0800

    增加了排序的测试

---
+++ Sort/sorttest.py
@@ -0,0 +1,17 @@
+from random import randint
+
+
+from Sort.mergesort import merge_sort
+from Sort.quicksort import quick_sort
+from Sort.bubblesort import hype_bubble_sort, sample_bubble_sort
+from Sort.selectsort import select_sort
+
+
+def test_mergesort(items, cmp = lambda x, y: x < y):
+    print(merge_sort(items, cmp))
+
+
+if __name__ == '__main__':
+    p = [randint(-10000, 10000) for _ in range(10000)]
+    print(p)
+    test_mergesort(p)
