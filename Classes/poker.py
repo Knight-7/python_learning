d2b6e68fd56638ed3c67212bed31ce96fff39fb6
knight <1252809618@qq.com>
2019-10-16 09:26:19 +0800

    2019-10-16 增加了Pork.py

--- Classes/poker.py
+++ Classes/poker.py
@@ -11,6 +11,7 @@ class Suite(Enum):
     SPADE, HEART, CLUB, DIAMOND = range(4)
 
     def __lt__(self, other):
+        """重载运算符，例：x < y 等价于x.__lt__(y)"""
         return self.value < other.value
 
 
@@ -28,6 +29,7 @@ class Card():
         faces = ['', 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
         return f'{suites[self.suite.value]}{faces[self.face]}'
 
+    """str和repr的区别：http://baijiahao.baidu.com/s?id=1596817611604972751&wfr=spider&for=pc"""
     def __str__(self):
         return self.show()
 
@@ -35,7 +37,58 @@ class Card():
         return self.show()
 
 
+class Poker():
+    """扑克"""
+
+    def __init__(self):
+        self.index = 0
+        self.cards = [Card(suite, face)
+                      for suite in Suite
+                      for face in range(1, 14)]
+
+    def shuffle(self):
+        """洗牌（将卡的顺序打乱）"""
+        random.shuffle(self.cards)
+
+    def deal(self):
+        """发牌"""
+        card = self.cards[self.index]
+        self.index += 1
+        return card
+
+    @property
+    def has_more(self):
+        return self.index < len(self.cards)
+
+
+class Player():
+    """玩家"""
+
+    def __init__(self, name):
+        self.name = name
+        self.cards = []
+
+    def one(self, card):
+        """摸一张牌"""
+        self.cards.append(card)
+
+    def sort(self, cmp=lambda card: (card.suite, card.face)):
+        """整理手中的牌"""
+        self.cards.sort(key=cmp)
+
+
+def main():
+    poker = Poker()
+    poker.shuffle()
+    players = [Player('东邪'), Player('西毒'), Player('南帝'), Player('北丐')]
+    while poker.has_more:
+        for player in players:
+            player.one(poker.deal())
+    for player in players:
+        player.sort()
+        print(f'name:{player.name}:', end=':')
+        print(player.cards)
+
+
 if __name__ == '__main__':
-    c = Card(1, 1)
-    print(c)
-    print(c.__repr__())
+    main()
