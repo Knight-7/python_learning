324c472e6677a8885307c4c8a5952138414eb8ff
knight <1252809618@qq.com>
2019-10-20 21:58:45 +0800

    2019-10-20 修改了MySQL连接类，添加一个数据可视化的文件

--- MySql/mysql_connect.py
+++ MySql/mysql_connect.py
@@ -5,34 +5,47 @@ class MysqlConeect():
     """自定义一个连接MySql的类"""
 
     def __init__(self, database, host='localhost', port=3306,
-                 charset='utf8', user='root', password='123456'):
-        self.host = host
-        self.port = port
-        self.database = database
-        self.charset = charset
-        self.user = user
-        self.password = password
-        self.connect = None
-        self.cursor = None
+                 charset='utf8', user='root', password='123456',
+                 is_close_auto=True):
+        self._host = host
+        self._port = port
+        self._database = database
+        self._charset = charset
+        self._user = user
+        self._password = password
+        self._connect = None
+        self._cursor = None
+        self._is_close_auto = is_close_auto
         self.connect_to_mysql()
 
     def connect_to_mysql(self):
-        self.connect = pymysql.connect(
-            host = self.host,
-            port = self.port,
-            database = self.database,
-            user = self.user,
-            password = self.password,
-            charset = self.charset
+        self._connect = pymysql.connect(
+            host = self._host,
+            port = self._port,
+            database = self._database,
+            user = self._user,
+            password = self._password,
+            charset = self._charset
         )
-        if self.connect:
-            self.cursor = self.connect.cursor()
+        if self._connect:
+            self._cursor = self.connect.cursor()
+
+    @property
+    def connect(self):
+        return self._connect
+
+    def creat_table(self, table):
+        pass
+
+    def insert(self, table, *args):
+        pass
 
     def __del__(self):
-        if self.connect:
-            self.connect.close()
-        if self.cursor:
-            self.cursor.close()
+        if self._is_close_auto:
+            if self._connect:
+                self._connect.close()
+            if self._cursor:
+                self._cursor.close()
 
 
 def main():
--- test.py
+++ test.py
@@ -1,32 +1,9 @@
-import asyncio
-import re
-
-import aiohttp
-
-PATTERN = re.compile(r'\<title\>(?P<title>.*)\<\/title\>')
-
-
-async def fetch_page(session, url):
-    async with session.get(url, ssl=False) as resp:
-        return await resp.text()
-
-
-async def show_title(url):
-    async with aiohttp.ClientSession() as session:
-        html = await fetch_page(session, url)
-        print(PATTERN.search(html).group('title'))
+import requests
 
 
 def main():
-    urls = ('https://www.python.org/',
-            'https://git-scm.com/',
-            'https://www.jd.com/',
-            'https://www.taobao.com/',
-            'https://www.douban.com/')
-    loop = asyncio.get_event_loop()
-    tasks = [show_title(url) for url in urls]
-    loop.run_until_complete(asyncio.wait(tasks))
-    loop.close()
+    r = requests.get('http://www.jianshu.com')
+    exit() if not r.status_code == requests.codes.ok else print('Request Successfully')
 
 
 if __name__ == '__main__':
