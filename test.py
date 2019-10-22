b020337cb431a29e3cb87651c5c3d43791f62aac
ouyuj <1252809618@qq.com>
2019-10-22 16:46:04 +0800

    2019-10-22 在Windows上修改了test.py文件

--- test.py
+++ test.py
@@ -1,16 +1,28 @@
-import  re
-from urllib.request import urljoin
+from bs4 import BeautifulSoup
 
 
 def main():
-    with open('html.txt', 'r') as f:
-        html = f.read()
-    pattern = re.compile('<li.*?href="(.*?)"\ssinger="(.*?)">(.*?)</a>', re.S)
-    results = re.findall(pattern, html)
-    base_url = 'https://www.netmusic.com'
-    for result in results:
-        full_url = urljoin(base_url, result[0])
-        print(f'歌手：{result[1]}, 歌曲：{result[2]}, 下载链接：{full_url}')
+    html = """
+    <html><head><title>The Dormouse's story</title></head>
+    <body>
+    <p class="title" name="dromouse"><b>The Dormouse's story</b></p>
+    <p class="story">Once upon a time there were three little sisters; and their names were
+    <a href="http://example.com/elsie" class="sister" id="link1"><!-- Elsie --></a>,
+    <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+    <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+    and they lived at the bottom of a well.</p>
+    <p class="story">...</p>
+    """
+    soup = BeautifulSoup(html, 'lxml')
+    print(soup.title)
+    print(type(soup.title))
+    print(soup.title.string)
+    print(soup.head)
+    print(type(soup.head))
+    print(soup.p)
+    print(soup.p.attrs)
+    print(type(soup.p.attrs))
+    print(soup.p.attrs['class'], soup.p['name'])
 
 
 if __name__ == '__main__':
