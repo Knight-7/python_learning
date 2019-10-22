f7543a17bac9e570329778cf8f2b2c354d495c01
knight <1252809618@qq.com>
2019-10-22 22:46:42 +0800

    2019-10-22 成功地从新笔趣可上爬下了圣墟

--- Web_Crawler/get_novel_shengxu.py
+++ Web_Crawler/get_novel_shengxu.py
@@ -1,6 +1,9 @@
 import re
 import requests
+import os
 
+from time import sleep
+from bs4 import BeautifulSoup
 from urllib.request import urljoin
 
 HEADERS = {
@@ -23,22 +26,47 @@ def get_novel_title(book_url):
             }
 
 
-def get_novel_text(name, url):
+def get_str(sentence):
+    p = 0
+    start = 0
+    for s in sentence:
+        if s != '\n' and s != ' ' and start == 0:
+            start = p
+        if start != 0 and s == '\n':
+            return sentence[start: p + 1]
+        p += 1
+
+
+def get_novel_text(url):
     response = requests.get(url, headers=HEADERS)
     response.encoding = 'utf8'
-    if response.status_code == 200:
-        pattern = re.compile("<div id='content'>.*?</div>")
-        results = re.findall(pattern, response.text)
-        for result in results:
-            print(result)
+    soup = BeautifulSoup(response.text, 'lxml')
+    html_text = soup.prettify()
+    pattern = re.compile('<br/>(.*?)<br/>|"content">(.*?)<br/>', re.S)
+    results = pattern.findall(html_text)
+    novel = get_str(results[0][1])
+    for result in results[1:-2]:
+        novel += get_str(result[0])
+    return novel
+
+
+def write_to_local(title, content):
+    path_file = '/home/knight/课程文件/Python/爬虫/圣墟/'
+    with open(path_file + title + '.txt', 'a') as f:
+        f.write(content)
 
 
 def main():
     base_url = 'http://www.xbiquge.la/13/13959'
     get_novel_title(base_url)
     for item in get_novel_title(base_url):
-        print(item['title'], item['link'])
+        print(f'正在下载----{item["title"]}')
+        novel = get_novel_text(item['link'])
+        write_to_local(item['title'], novel)
+        sleep(0.2)
 
 
 if __name__ == '__main__':
-    get_novel_text('123', 'http://www.xbiquge.la/13/13959/20171982.html')
+    path = '/home/knight/课程文件/Python/爬虫'
+    os.mkdir(path + '/圣墟')
+    main()
--- test.py
+++ test.py
@@ -1,29 +1,42 @@
+import re
+import requests
+import os
+
 from bs4 import BeautifulSoup
+
+HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/52.0.2743.116 Safari/537.36'
+    }
+
+
+def get_str(sentence):
+    p = 0
+    start = 0
+    for s in sentence:
+        if s != '\n' and s != ' ' and start == 0:
+            start = p
+        if start != 0 and s == '\n':
+            return sentence[start: p + 1]
+        p += 1
 
 
 def main():
-    html = """
-    <html><head><title>The Dormouse's story</title></head>
-    <body>
-    <p class="title" name="dromouse"><b>The Dormouse's story</b></p>
-    <p class="story">Once upon a time there were three little sisters; and their names were
-    <a href="http://example.com/elsie" class="sister" id="link1"><!-- Elsie --></a>,
-    <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-    <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-    and they lived at the bottom of a well.</p>
-    <p class="story">...</p>
-    """
-    soup = BeautifulSoup(html, 'lxml')
-    print(soup.title)
-    print(type(soup.title))
-    print(soup.title.string)
-    print(soup.head)
-    print(type(soup.head))
-    print(soup.p)
-    print(soup.p.attrs)
-    print(type(soup.p.attrs))
-    print(soup.p.attrs['class'], soup.p['name'])
+    url = 'http://www.xbiquge.la/13/13959/20185420.html'
+    response = requests.get(url, headers=HEADERS)
+    response.encoding = 'utf8'
+    soup = BeautifulSoup(response.text, 'lxml')
+    html_text = soup.prettify()
+    pattern = re.compile('<br/>(.*?)<br/>|"content">(.*?)<br/>', re.S)
+    results = pattern.findall(html_text)
+    novel = get_str(results[0][1])
+    for result in results[1:-2]:
+        novel += get_str(result[0])
+    return novel
 
 
 if __name__ == '__main__':
-    main()
+    path = '/home/knight/课程文件/Python/爬虫/圣墟/'
+    with open(path + '123' + '.txt', 'a') as f:
+        f.write(main())
